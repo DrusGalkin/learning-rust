5ef8c94c728ddf72e0e42d25e9e0d2933145d5bf
DrusGalkin <sveta.galkina2021@gmail.com>
2026-01-14 17:22:58 +0300

    ignore idea

---
+++ base-syntax/less1/target/debug/deps/less1-0fdc213326fc2220.d
@@ -0,0 +1,5 @@
+C:\Users\AndrewGalkin\RustroverProjects\untitled\base-syntax\less1\target\debug\deps\less1-0fdc213326fc2220.d: src\main.rs
+
+C:\Users\AndrewGalkin\RustroverProjects\untitled\base-syntax\less1\target\debug\deps\libless1-0fdc213326fc2220.rmeta: src\main.rs
+
+src\main.rs:
---
+++ base-syntax/less1/target/debug/deps/less1-8f25cef2435dbce9.d
@@ -0,0 +1,5 @@
+C:\Users\AndrewGalkin\RustroverProjects\untitled\base-syntax\less1\target\debug\deps\less1-8f25cef2435dbce9.d: src\main.rs
+
+C:\Users\AndrewGalkin\RustroverProjects\untitled\base-syntax\less1\target\debug\deps\libless1-8f25cef2435dbce9.rmeta: src\main.rs
+
+src\main.rs:
