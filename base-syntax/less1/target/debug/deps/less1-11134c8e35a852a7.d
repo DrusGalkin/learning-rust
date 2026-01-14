7e457bc9490ca5913b1b3a2bc902c154545a93da
DrusGalkin <sveta.galkina2021@gmail.com>
2026-01-14 17:21:43 +0300

    first commit

---
+++ base-syntax/less1/target/debug/deps/less1-11134c8e35a852a7.d
@@ -0,0 +1,5 @@
+C:\Users\AndrewGalkin\RustroverProjects\untitled\base-syntax\less1\target\debug\deps\less1-11134c8e35a852a7.d: src\main.rs
+
+C:\Users\AndrewGalkin\RustroverProjects\untitled\base-syntax\less1\target\debug\deps\less1-11134c8e35a852a7.exe: src\main.rs
+
+src\main.rs:
---
+++ base-syntax/less1/target/debug/less1.d
@@ -0,0 +1,1 @@
+C:\Users\AndrewGalkin\RustroverProjects\untitled\base-syntax\less1\target\debug\less1.exe: C:\Users\AndrewGalkin\RustroverProjects\untitled\base-syntax\less1\src\main.rs
